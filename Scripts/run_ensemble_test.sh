40fe4396b2653175bafa92e8cc99216a0318cdae
Guillermo Cossio <guillermo.cossio@imt-atlantique.fr>
2026-01-14 12:25:32 +0100

    Added ensemble configs and analysis notebook

---
+++ Scripts/run_ensemble_test.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+
+# Define the list of IDs
+IDS=(
+  # control
+  sigma_0.5
+  sigma_1.0
+  sigma_2.0
+  # sigma_5.0 # Unstable on some runs
+)
+
+# Loop over IDs and run the command
+for ID in "${IDS[@]}"; do
+  echo "Running ensemble: ${ID}"
+  python src/noisy_ensemble.py --config "./configs/ENS-${ID}.yaml"
+done
